f8f639bc78b21e427ea82bd6ba4b24319b8cfa6a
arpitkatiyar1999 <arpitkatiyar2018@gmail.com>
2022-09-13 18:25:25 +0530

    Lesson-2

--- app/src/main/java/com/example/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/com/example/kotlincoroutines/MainActivity.kt
@@ -25,13 +25,20 @@ class MainActivity : AppCompatActivity() {
 //                val bitmap=BitmapFactory.decodeStream(url.openStream())
 //                imageView.setImageBitmap(bitmap)
                 CoroutineScope(Dispatchers.IO).launch {
-                    Log.e("thread","Thread ${Thread.currentThread().name}")
-                    val url=URL("https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGhvdG98ZW58MHx8MHx8&w=1000&q=80")
-                    val bitmap=BitmapFactory.decodeStream(url.openStream())
-                    withContext(Dispatchers.Main)
+                    try {
+                        Log.e("thread", "Thread ${Thread.currentThread().name}")
+                        val url =
+                            URL("https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGhvdG98ZW58MHx8MHx8&w=1000&q=80")
+                        val bitmap = BitmapFactory.decodeStream(url.openStream())
+                        withContext(Dispatchers.Main)
+                        {
+                            Log.e("thread", "Thread ${Thread.currentThread().name}")
+                            imageView.setImageBitmap(bitmap)
+                        }
+                    }
+                    catch(e:Exception)
                     {
-                        Log.e("thread","Thread ${Thread.currentThread().name}")
-                        imageView.setImageBitmap(bitmap)
+                        Log.e("error",e.message!!)
                     }
                 }
             }
